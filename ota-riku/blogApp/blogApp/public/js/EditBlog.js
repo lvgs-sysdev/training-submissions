740a5cb02786634ab0e4a9583f8fa71e27411e23
riku-ota-lvgs <riku.ota@leverages.jp>
2025-06-09 17:49:24 +0900

    html読み込み用のjsを機能ごとではなくページごとに変更

--- ota-riku/blogApp/blogApp/public/js/EditBlog.js
+++ ota-riku/blogApp/blogApp/public/js/EditBlog.js
@@ -1,3 +1,34 @@
+// 記事表示処理
+document.addEventListener("DOMContentLoaded", async () => {
+  const url = new URL(window.location.href);
+  const path = url.pathname.split("/");
+  const id = path[path.length - 1];
+
+  const titleInputElem = document.getElementById("title-input");
+  const contextInputElem = document.getElementById("context-input");
+
+  try {
+    const response = await fetch("/api/article-info", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        articleId: id,
+      }),
+    });
+    if (response.ok) {
+      const articleInfo = await response.json();
+      console.log(articleInfo);
+      titleInputElem.value = articleInfo.article_title;
+      contextInputElem.textContent = articleInfo.content;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+// 編集完了ボタン処理
 const editblogForm = document.getElementById("edit-blog-form");
 
 editblogForm.addEventListener("submit", async (event) => {
--- ota-riku/blogApp/blogApp/public/js/EditUser.js
+++ ota-riku/blogApp/blogApp/public/js/EditUser.js
@@ -1,3 +1,4 @@
+// 入力チェックと変更処理
 const editForm = document.getElementById("edit-user-form");
 const errorMsgElem = document.getElementById("error-msg");
 
@@ -74,3 +75,18 @@ const CreateErrorMsgElem = (errorMsg) => {
   errorMsgElem.appendChild(errorMsgTextElem);
   return errorMsgTextElem;
 };
+
+// ユーザー情報の読み込み
+document.addEventListener("DOMContentLoaded", () => {
+  const idTextElement = document.getElementById("userIdText");
+  const nameTextElement = document.getElementById("userNameText");
+  getUserInfo();
+
+  async function getUserInfo() {
+    const response = await fetch("/api/user-info");
+    const userInfo = await response.json();
+    console.log(userInfo);
+    idTextElement.innerText = userInfo.userId;
+    nameTextElement.innerText = userInfo.userName;
+  }
+});
