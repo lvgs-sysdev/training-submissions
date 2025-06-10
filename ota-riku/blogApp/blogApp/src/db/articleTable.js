eebf13ace0769175a958335aa0290d5b7e8f67aa
riku-ota-lvgs <riku.ota@leverages.jp>
2025-06-10 18:34:37 +0900

    データベース処理のtry/catch

--- ota-riku/blogApp/blogApp/src/db/articleTable.js
+++ ota-riku/blogApp/blogApp/src/db/articleTable.js
@@ -2,36 +2,53 @@ import mysql from "mysql2/promise";
 import { dbConfig } from "./config.js";
 
 export const applyArticle = async (userId, title, context) => {
-  const connection = await mysql.createConnection(dbConfig);
-  const currentDate = new Date();
-  await connection.query(
-    "INSERT INTO articles (user_id,article_title,content,updated_at) VALUES (?, ?, ?, ?)",
-    [userId, title, context, currentDate],
-    (error) => {
-      if (error) throw error;
-    }
-  );
-  (await connection).end();
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const currentDate = new Date();
+    await connection.query(
+      "INSERT INTO articles (user_id,article_title,content,updated_at) VALUES (?, ?, ?, ?)",
+      [userId, title, context, currentDate],
+      (error) => {
+        if (error) throw error;
+      }
+    );
+    (await connection).end();
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 export const getNewArticle = async (numArticle) => {
-  const connection = await mysql.createConnection(dbConfig);
-  const [raws, fields] = await connection.query(
-    "SELECT * FROM articles ORDER BY updated_at DESC LIMIT ?",
-    [numArticle]
-  );
-  (await connection).end();
-  return [raws, fields];
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const [raws, fields] = await connection.query(
+      "SELECT * FROM articles ORDER BY updated_at DESC LIMIT ?",
+      [numArticle],
+      (error) => {
+        if (error) throw error;
+      }
+    );
+    (await connection).end();
+    return [raws, fields];
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
 };
 
 export const getArticleInfo = async (articleId) => {
-  const connection = await mysql.createConnection(dbConfig);
-  const [raws, fields] = await connection.query(
-    "SELECT * FROM articles WHERE article_id = ?",
-    [articleId]
-  );
-  (await connection).end();
-  return raws;
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const [raws, fields] = await connection.query(
+      "SELECT * FROM articles WHERE article_id = ?",
+      [articleId]
+    );
+    (await connection).end();
+    return raws;
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
 };
 
 export const editArticle = async (
@@ -39,14 +56,18 @@ export const editArticle = async (
   newArticleTitle,
   newArticleContext
 ) => {
-  const connection = await mysql.createConnection(dbConfig);
-  const currentDate = new Date();
-  await connection.query(
-    "UPDATE articles SET article_title=?,content=?,updated_at=? WHERE article_id=?",
-    [newArticleTitle, newArticleContext, currentDate, articleId],
-    (error) => {
-      if (error) throw error;
-    }
-  );
-  await connection.end;
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const currentDate = new Date();
+    await connection.query(
+      "UPDATE articles SET article_title=?,content=?,updated_at=? WHERE article_id=?",
+      [newArticleTitle, newArticleContext, currentDate, articleId],
+      (error) => {
+        if (error) throw error;
+      }
+    );
+    await connection.end;
+  } catch (e) {
+    console.log(e);
+  }
 };
