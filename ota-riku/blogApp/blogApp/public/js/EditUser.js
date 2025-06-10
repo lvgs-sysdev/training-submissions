809d089c388fedb365862c738bb52ea66dc9a619
riku-ota-lvgs <riku.ota@leverages.jp>
2025-06-10 11:35:23 +0900

    条件分岐の追加

--- ota-riku/blogApp/blogApp/public/js/EditUser.js
+++ ota-riku/blogApp/blogApp/public/js/EditUser.js
@@ -24,46 +24,48 @@ editForm.addEventListener("submit", async (event) => {
     try {
       // userIdが元々の自分のuserIdの場合
       const responseMyIdCheck = await fetch("/api/user-info");
-      const currentMyUserId = await responseMyIdCheck.json();
-
-      // id重複チェック
-      const responseIdCheck = await fetch("/api/user-info", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          userId: userIdElem.value,
-        }),
-      });
-      if (responseIdCheck.ok) {
-        const userInfo = await responseIdCheck.json();
-        if (
-          userInfo.userId != "" &&
-          userInfo.userId != currentMyUserId.userId
-        ) {
-          CreateErrorMsgElem("このユーザIDは既に使われています。");
-        } else {
-          // 問題なければ登録情報を送信してログイン画面に遷移
-          // 登録情報送信
-          const responseRegist = await fetch("/editUser", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              user_id: userIdElem.value,
-              user_name: usernameElem.value,
-            }),
-          });
-          if (responseRegist.ok) {
-            // 画面遷移
-            window.location.href = "/user";
+      if (responseMyIdCheck.ok) {
+        const currentMyUserId = await responseMyIdCheck.json();
+        // id重複チェック
+        const responseIdCheck = await fetch("/api/user-info", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userId: userIdElem.value,
+          }),
+        });
+        if (responseIdCheck.ok) {
+          const userInfo = await responseIdCheck.json();
+          if (
+            userInfo.userId != "" &&
+            userInfo.userId != currentMyUserId.userId
+          ) {
+            CreateErrorMsgElem("このユーザIDは既に使われています。");
+          } else {
+            // 問題なければ登録情報を送信してログイン画面に遷移
+            // 登録情報送信
+            const responseRegist = await fetch("/editUser", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                user_id: userIdElem.value,
+                user_name: usernameElem.value,
+              }),
+            });
+            if (responseRegist.ok) {
+              // 画面遷移
+              window.location.href = "/user";
+            }
           }
         }
       }
     } catch (e) {
       console.log(e);
+      CreateErrorMsgElem("サーバーエラーが発生しました。");
     }
   }
 });
