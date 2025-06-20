4e01774bf1950706034975b77ecd81a0d6e6cab3
riku-ota-lvgs <riku.ota@leverages.jp>
2025-06-20 15:43:36 +0900

    ヘッダーの実装

---
+++ ota-riku/twitter-like/frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import { Header } from "./compornents/header";
+export const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
---
+++ ota-riku/twitter-like/frontend/src/compornents/header.tsx
@@ -0,0 +1,56 @@
+import { FC } from "react";
+
+// TODO: アイコンのパスを指定してあげる
+export const Header: FC = () => {
+  const headerStyle = {
+    alignItems: "center",
+    margin: "0",
+    padding: "0",
+    display: "flex",
+    height: "90px",
+    width: "1000px",
+    backgroundColor: "black",
+    zIndex: "1",
+    textAlign: "center",
+  } as React.CSSProperties;
+
+  const logoStyle = {
+    fontSize: "50px",
+    color: "red",
+    textAlign: "center",
+    marginLeft: "auto",
+    marginRight: "auto",
+  } as React.CSSProperties;
+
+  const iconStyle = {
+    alignContent: "center",
+    width: "50px",
+    height: "50px",
+    objectFit: "cover",
+    borderRadius: "50%",
+    paddingLeft: "20px",
+  } as React.CSSProperties;
+
+  const notificateIcon = {
+    alignContent: "center",
+    width: "50px",
+    height: "50px",
+    paddingRight: "20px",
+  };
+
+  return (
+    <>
+      <header style={headerStyle}>
+        <img
+          src={`${process.env.PUBLIC_URL}/images/cat_meam.jpg`}
+          style={iconStyle}
+        />
+        <h1 style={logoStyle}>R</h1>
+        <img
+          src={`${process.env.PUBLIC_URL}/images/bell.png`}
+          style={notificateIcon}
+        />
+      </header>
+    </>
+  );
+};
