e92c753afb9240c9dd346ef2738b7ae401a5a48b
tatsuro-kawakami-lvgs <tatsuro.kawakami@leverages.jp>
2025-11-11 12:43:01 +0900

    fix: レビュー対応 - ヘッダー・フッターを共通HTMLファイル化 - ヘッダー・フッターをfetchするJSの実装 - その他ヘッダー・フッターのクラス名の修正等

---
+++ kawakami-tatsuro/html-css-submission/scripts/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// 第一引数で受け取ったセレクターに一致するDOM要素を、第二引数で受け取ったHTMLで置換する関数
+const loadComponent = async (selector, path) => {
+  const targetElement = document.querySelector(selector);
+  if (!targetElement) return; // 要素がなければ何もしない
+
+  try {
+    const response = await fetch(path);
+    if (!response.ok) throw new Error();
+
+    const html = await response.text();
+
+    targetElement.innerHTML = html;
+  }
+
+  catch(error) {
+    targetElement.innerHTML = '<p>DAYDAY</p><p>コンテンツの読み込みに失敗しました。ページを再読み込みしてください。</p>'
+  }
+}
+
+// DOMが読み込まれたらヘッダー・フッターをcommonのHTMLファイルで置換するイベントリスナー
+document.addEventListener('DOMContentLoaded', () => {
+  loadComponent('.common-header', '../html/common/header.html');
+  loadComponent('.common-footer', '../html/common/footer.html');
+});
