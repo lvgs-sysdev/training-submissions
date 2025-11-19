9196aa3516ea3b21423228b96a33f007cd2672fb
tatsuro-kawakami-lvgs <tatsuro.kawakami@leverages.jp>
2025-11-19 14:07:05 +0900

    feat: 記事詳細画面の実装まで完了

--- kawakami-tatsuro/blog-submission/public/js/utils.js
+++ kawakami-tatsuro/blog-submission/public/js/utils.js
@@ -27,3 +27,80 @@ export const formatDate = (date) => {
 
   return jstDate;
 }
+
+// 現在表示中のページのURLから引数に一致するパラメーターの値を取得する
+export const getParamsFromCurrentUrl = (paramName) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(paramName)
+}
+
+// articleのジャンル、日付を格納したDOM要素を作成する
+export const createMetaBox = (article) => {
+  const metaBox = document.createElement("div");
+  const genre = document.createElement("span");
+  const hr = document.createElement("hr");
+  const date = document.createElement("span");
+
+  metaBox.classList.add("article-list__col__item__desc");
+  genre.classList.add("article-list__col__item__desc-genre");
+  hr.classList.add("article-list__col__item__desc-hr");
+  date.classList.add("article-list__col__item__desc-date");
+
+  genre.textContent = article.genre;
+  date.textContent = formatDate(article.updated_at);
+
+  metaBox.appendChild(genre);
+  metaBox.appendChild(hr);
+  metaBox.appendChild(date);
+
+  return metaBox;
+}
+
+// articleのサムネイル画像、タイトル、本文を含んだDOM要素を作成し、オブジェクトとして返却する
+export const createArticleMainData = (article) => {
+  const img = document.createElement("img");
+  const title = document.createElement("h4");
+  const content = document.createElement("p");
+
+  img.setAttribute("src", article.thumbnail_path);
+  img.setAttribute("alt", "サムネイル画像");
+  title.classList.add("article-list__col__item-title");
+  content.classList.add("article-list__col__item-text");
+  
+  title.textContent = article.article_title;
+  content.innerHTML = article.content;
+  
+  return {img, title, content};
+}
+
+// 個々のarticleのボックスのDOM要素を作成する
+export const createArticleListElement = (article) => {
+  const itemBox = document.createElement("div");
+  const articleDetailLink = document.createElement("a");
+  
+  const {img, title, content} = createArticleMainData(article);
+  const metaBox = createMetaBox(article);
+
+  itemBox.classList.add("article-list__col__item");
+  articleDetailLink.classList.add("article-list__col__item__link");
+  articleDetailLink.setAttribute("href", `detail.html?id=${article.id}`);
+  
+  articleDetailLink.appendChild(img);
+  articleDetailLink.appendChild(metaBox);
+  articleDetailLink.appendChild(title);
+  articleDetailLink.appendChild(content);
+
+  itemBox.appendChild(articleDetailLink);
+
+  return itemBox;
+}
+
+// articles全件をループ処理し、記事要素をDOMに追加する
+export const createArticleList = (articles, id) => {
+  const articleList = document.getElementById(id);
+
+  articles.forEach((article) => {
+    const itemBox = createArticleListElement(article);
+    articleList.appendChild(itemBox);
+  });
+};
--- kawakami-tatsuro/blog-submission/src/server.js
+++ kawakami-tatsuro/blog-submission/src/server.js
@@ -1,33 +1,65 @@
-'use strict';
+"use strict";
 
-const fastify = require('fastify')({logger: true});
-const path = require('path');
-const fastifyStatic = require('@fastify/static');
-const db = require('./db/db');
+const fastify = require("fastify")({ logger: true });
+const path = require("path");
+const fastifyStatic = require("@fastify/static");
+const db = require("./db/db");
 
 fastify.register(fastifyStatic, {
-  root: path.join(__dirname, '../public')
-})
+  root: path.join(__dirname, "../public"),
+});
 
 // articles全件を返すAPIエンドポイント（更新日時が新しい順）
-fastify.get('/articles', async () => {
-  const data = await db.query('SELECT * FROM articles ORDER BY updated_at DESC');
-  const allArticles = data[0];
-  return allArticles;
-})
+fastify.get("/articles", async () => {
+  const [rows] = await db.query(
+    "SELECT * FROM articles ORDER BY updated_at DESC"
+  );
 
-// URLから受け取ったidのarticleのデータを返すAPIエンドポイント
-fastify.get('/article/:id', async  (request, reply) => {
+  return rows;
+});
+
+// URLから受け取ったidのarticleのデータと著者のデータを返すAPIエンドポイント
+fastify.get("/article/:id", async (request) => {
   const id = request.params.id;
-  const [rows] = await db.query('SELECT * FROM articles WHERE id = :id', { id: id });
+  const [rows] = await db.query(`
+    SELECT
+      articles.id,
+      articles.article_title,
+      articles.genre, articles.content,
+      articles.updated_at,
+      articles.thumbnail_path,
+      users.user_id, users.user_name,
+      users.icon_path
+    FROM
+      articles
+    INNER JOIN
+      users ON articles.user_id = users.user_id
+    WHERE
+      articles.id = :id
+    `,
+    { id: id }
+  );
+
   return rows[0];
-})
+});
+
+fastify.get("/users", async () => {
+  const [rows] = await db.query(`
+    SELECT
+      id,
+      user_name,
+      icon_path
+    FROM
+      users
+    `);
+
+    return rows;
+});
 
 const start = async () => {
   try {
     await fastify.listen({ port: 3000 });
-  }
-  catch (err) {
+  } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
