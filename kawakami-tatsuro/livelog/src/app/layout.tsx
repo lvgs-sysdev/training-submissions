8ece88d6fd76e7a623d4797bff197cd71f456bd2
tatsuro-kawakami-lvgs <tatsuro.kawakami@leverages.jp>
2025-12-09 13:02:22 +0900

    add: Tailwind CSS追加、フォント変更

--- kawakami-tatsuro/livelog/src/app/layout.tsx
+++ kawakami-tatsuro/livelog/src/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
+import { notoSansJp } from "@/lib/fonts";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={notoSansJp.variable}>
+      <body className="antialiased flex flex-col min-h-screen">
+        <header className="p-4"><p className="text-4xl">header</p></header>
+        <main className="p-4 md:p-8 flex-1 max-w-7xl mx-auto w-full">
         {children}
+        </main>
+        <footer><p>footer</p></footer>
       </body>
     </html>
   );
