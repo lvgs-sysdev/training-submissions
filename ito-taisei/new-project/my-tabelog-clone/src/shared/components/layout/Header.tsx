e955669a6725204018acd5ed2fe978f12c3201c8
taisei.ito <taisei.ito@leverages.jp>
2025-12-18 16:40:58 +0900

    たいぽ修正

--- ito-taisei/new-project/my-tabelog-clone/src/shared/components/layout/Header.tsx
+++ ito-taisei/new-project/my-tabelog-clone/src/shared/components/layout/Header.tsx
@@ -11,7 +11,7 @@ export default function Header({ session }: HeaderProps) {
   return (
     <header className="fixed top-0 left-0 w-full z-50 h-16 min-h-16 flex items-center justify-between px-8 border-b border-gray-200 bg-white bg-opacity-95 backdrop-blur">
       <Link href="/" className="no-underline text-[20px] font-bold text-[#d9534f]">
-        Taberogu Clone
+        Tabelog Clone
       </Link>
 
       <nav className="flex items-center gap-5 h-full">
