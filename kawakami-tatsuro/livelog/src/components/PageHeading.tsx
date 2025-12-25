2b6717209a07411f64ed2c5babfb45a53f7b7e5a
tatsuro-kawakami-lvgs <tatsuro.kawakami@leverages.jp>
2025-12-25 16:39:42 +0900

    feat: ナビゲーションメニュー、ページ見出し

--- kawakami-tatsuro/livelog/src/components/PageHeading.tsx
+++ kawakami-tatsuro/livelog/src/components/PageHeading.tsx
@@ -1,9 +1,11 @@
+import { zalandSansExpanded } from "@/lib/fonts"
+
 interface Props {
   heading: string
 }
 
 export const PageHeading = ({ heading }: Props) => {
   return (
-    <h2 className="mb-8 text-4xl font-bold">{heading}</h2>
+    <h2 className={`${zalandSansExpanded.variable} font-zaland-sans-expanded mb-8 text-3xl font-bold`}>{heading}</h2>
   )
 }
