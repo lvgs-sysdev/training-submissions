8c86d8d15883bcd8fcd6be41177346ff35fd337f
motoki-harunari-lvgs <motoki.harunari@leverages.jp>
2026-04-10 15:10:09 +0900

    ここまででフロントエンドとバックエンドの処理終了（サーバ立ち上げを除く）

---
+++ harunari-motoki/Nature-Lens/src/FrontendScript/Viewmodel/scanEventBridge.ts
@@ -0,0 +1,38 @@
+import { GBIFdetailInfo } from "@/sharedObject/typeDiffinition.ts";
+import { ClientSideControl } from "../APIservice/GeolocationTrafficControl";
+
+const scanButton = document.querySelector(".scan_button");
+
+if (!scanButton) {
+  // 【オプション】サーバサイドにログを送りたい
+  alert("処理が正常に終了しませんでした。再度実行してください。");
+} else {
+  scanButton.addEventListener("click", async (_) => {
+    const response = await ClientSideControl();
+    try {
+      if (response.status === "success") {
+        const { count } = response.data;
+        const { results } = response.data;
+
+        const scanResultContainer = document.querySelector(
+          ".scanresult-container",
+        );
+        if (scanResultContainer) {
+          scanResultContainer.innerHTML = `<p>スキャン結果：${count}</p><br>`;
+          //今後長くなれば関数として切り取る
+          results.forEach((results: GBIFdetailInfo) => {
+            scanResultContainer.innerHTML += `
+              <p>種名：${results.species}</p><br>
+              <p>観察年月日：${results.year}/${results.month}/${results.day}</p><br>`;
+          });
+        }
+      } else {
+        const { message } = response;
+        alert(message);
+      }
+    } catch (error) {
+      const message = "エラーが発生しました。再度実行してください。";
+      alert(message);
+    }
+  });
+}
