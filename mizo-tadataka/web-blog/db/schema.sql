15af8129bd6854114110be1a78cdf1ae8cc193db
tadataka-mizo-lvgs <tadataka.mizo@leverages.jp>
2026-04-24 21:05:31 +0900

    blog_login_register

--- mizo-tadataka/web-blog/db/schema.sql
+++ mizo-tadataka/web-blog/db/schema.sql
@@ -1,14 +1,15 @@
 -- テーブル設計
 -- ユーザーの基本設定
 CREATE TABLE users(
-    user_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(20) NOT NULL,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(20) NOT NULL,
+    username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password varchar(255) NOT NULL,  
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     -- ルール
-    UNIQUE KEY (username),
+    UNIQUE KEY (user_id),
     UNIQUE KEY (email)
    
 );
@@ -16,11 +17,11 @@ CREATE TABLE users(
 CREATE TABLE articles(
     article_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     content TEXT not NULL,
-    user_id INT UNSIGNED NOT NULL,
+    id INT UNSIGNED NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     -- ルール
-    FOREIGN KEY (user_id) REFERENCES  users(user_id) ON DELETE CASCADE
+    FOREIGN KEY (id) REFERENCES  users(id) ON DELETE CASCADE
 );
 -- SNS一覧
 CREATE TABLE sns_types(
@@ -32,12 +33,12 @@ CREATE TABLE sns_types(
 -- ユーザーのSNS
 CREATE TABLE sns_links(
     sns_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    user_id INT UNSIGNED NOT NULL,
+    id INT UNSIGNED NOT NULL,
     sns_type_id INT UNSIGNED NOT NULL,
     sns_url VARCHAR(255) NOT NULL,
     -- ルール
-    UNIQUE KEY unique_user_sns (user_id, sns_type_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    UNIQUE KEY unique_user_sns (id, sns_type_id),
+    FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (sns_type_id) REFERENCES sns_types(sns_type_id)
 );
 -- タグ一覧
--- mizo-tadataka/web-blog/server.js
+++ mizo-tadataka/web-blog/server.js
@@ -1,32 +1,110 @@
 //環境の構築
 //ライブラリ導入と設定、ツールの導入
+require("dotenv").config();
 const fastify = require("fastify")({ logger: true });
-const { default: fastifyMysql } = require("@fastify/mysql");
+const fastifyMysql = require("@fastify/mysql");
+const fastifyStatic = require("@fastify/static");
 const path = require("path");
+const argon2 = require("argon2");
+const formBody = require("@fastify/formbody");
+const cookie = require("@fastify/cookie");
+
 //プラグイン（拡張パーツ）の設定
-//フロント側の設定（ユーザーに見えるファイル）
-fastify.register(require("@fastify/static"), {
+fastify.register(fastifyStatic, {
   root: path.join(__dirname, "public"),
 });
-//mysqlとfastifyの設定
-fastify.register(require("@fastify/mysql"), {
+fastify.register(fastifyMysql, {
   promise: true,
-  connectionString: "mysql://root:@localhost/blog",
+  connectionString: process.env.DB_URL,
 });
+
+fastify.register(formBody);
+fastify.register(require("@fastify/cookie"), {
+  secret: process.env.COOKIE_SECRET,
+});
+
 //プログラム開始
-//mysqlとのやりとりの確認
-fastify.get("/api/v1/articles", async (request, reply) => {
+//テスト
+fastify.get("/api/v1/user", async (request, reply) => {
   const [rows] = await fastify.mysql.query(
     "SELECT * FROM tweet ORDER BY created_at DESC",
   );
   return rows;
 });
 
+//データの送信(新規登録)
+fastify.post("/api/v1/register", async (request, reply) => {
+  try {
+    const { user_id, username, email, password } = request.body;
+    const hashedPassword = await argon2.hash(password);
+    await fastify.mysql.query(
+      "INSERT INTO users (user_id, username, email, password) VALUES (?, ?, ?, ?)",
+      [user_id, username, email, hashedPassword],
+    );
+
+    return reply.send({ message: "ユーザー登録完了！" });
+  } catch (err) {
+    fastify.log.error(err);
+    if (error.code == "ER_DUP_ENTRY") {
+      return reply
+        .code(400)
+        .send({ message: "すでにID、メールアドレスが使われています。" });
+    }
+    return reply.code(500).send({ message: "エラーです" });
+  }
+});
+
+//データの照合および認証(ログイン)
+fastify.post("/api/v1/login", async (request, reply) => {
+  try {
+    const { identifier, password } = request.body;
+    //Dataを取ってくる
+    const [users] = await fastify.mysql.query(
+      "SELECT * FROM users WHERE email = ? OR user_id = ?",
+      [identifier, identifier],
+    );
+    //検索結果が引っかからなかったとき
+    if (users.length === 0) {
+      return reply
+        .code(401)
+        .send({ message: "ユーザーが見つかりませんでした。" });
+    }
+
+    const user = users[0];
+
+    //パスワード検証
+    const isMatch = await argon2.verify(user.password, password);
+
+    if (!isMatch) {
+      return reply
+        .code(401)
+        .send({ message: " ユーザー名もしくはパスワードが異なります。" });
+    }
+
+    //クッキーの紐づけと設定、および発行
+    reply.setCookie("userid", String(user.id), {
+      path: "/",
+      signed: true,
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 30,
+    });
+    return {
+      success: true, //成功フラグ
+      message: "ログイン成功",
+      username: user.username,
+    };
+  } catch (err) {
+    fastify.log.error(err);
+    return reply.code(500).send({ message: "サーバーエラー" });
+  }
+});
+
 //サーバーの起動
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
-    console.log("サーバーが3000番にて開始");
+    const port = process.env.PORT || 3000;
+    await fastify.listen({ port: port });
+    console.log(`サーバーが ${port} 番にて開始`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
